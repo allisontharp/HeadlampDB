2d6b2e82da35f64eac5010da875b609004804f20
allisontharp <allison.tharp@gmail.com>
2015-06-23 14:35:04 -0400

    Update mysqli_headlamp_connect.php

--- mysqli_headlamp_connect.php
+++ mysqli_headlamp_connect.php
@@ -1,8 +1,8 @@
 <?php
-DEFINE ('DB_USER', 'web');
-DEFINE ('DB_PASSWORD', 'zsxdcf');
+DEFINE ('DB_USER', 'username');
+DEFINE ('DB_PASSWORD', 'pswd');
 DEFINE ('DB_HOST', '76.74.170.191');
-DEFINE ('DB_NAME', 'vehicles');
+DEFINE ('DB_NAME', 'dbname');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL ' .
