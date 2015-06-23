5144f6087627661a5a58b3eb18448114084ce457
Allison Tharp <allison.tharp@gmail.com>
2015-06-23 18:01:54 +0000

    Adding addheadlamp.php

---
+++ addheadlamp.php
@@ -0,0 +1,48 @@
+
+<html>
+<head>
+<title>Add Headlamp</title>
+</head>
+<body>
+
+<form action = "http://76.74.170.191/headlampadded.php" method = "post">
+<b> Add a New Headlamp</b>
+
+<p>Year:
+<input type = "text" name="year" size = "30" value = "" />
+</p>
+
+<p>Make:
+<input type = "text" name = "make" size = "30" value = "" />
+</p>
+
+<p>Model:
+<input type = "text" name = "model" size = "30" value = "" />
+</p>
+
+<p>Style:
+<input type = "text" name = "style" size = "30" value = "" />
+</p>
+
+<p>Low Beam Bulb:
+<input type = "text" name = "lowbeam" size = "30" value = "" />
+</p>
+
+<p>Low Beam Technology:
+<input type = "text" name = "lbtech" size = "30" value = "" />
+</p>
+
+<p>High Beam Bulb:
+<input type = "text" name = "highbeam" size = "30" value = "" />
+</p>
+
+<p>High Beam Technology:
+<input type = "text" name = "hbtech" size = "30" value = ""/>
+</p>
+
+<p>
+<input type = "submit" name = "submit" value = "Send" />
+
+</form>
+</body>
+</html>
